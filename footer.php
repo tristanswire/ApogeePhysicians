5a72beaa2bd71e46c999895a4a4fcc349ec7d2d4
Tristan Swire <tristanswire@gmail.com>
2017-09-20 14:48:49 -0500

    Updates

--- footer.php
+++ footer.php
@@ -30,8 +30,8 @@
 
 <?php wp_footer(); ?>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>_js/functions-min.js"></script>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -16,12 +16,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style.css"></link>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="page" class="site site-wrap">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'apogee-physicians' ); ?></a>
 
 	<header id="masthead" class="site-header">
